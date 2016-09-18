c72b55705d892e6e58dc67773d1c4466816c1239
alexpadraic <alex.padraic@gmail.com>
2016-09-18 09:08:37 -0700

    Add query for latlong point

--- app/controllers/crimepoints_controller.rb
+++ app/controllers/crimepoints_controller.rb
@@ -5,10 +5,14 @@ class CrimepointsController < ApplicationController
 
 	def show
 
-		@location = params[:search]
-		@latlng = Crimepoint.geolocation(@location)
-		@all_crimepoints = Crimepoint.return_crimepoints.to_json
-		
+		location = params[:search]
+		@latlng = Crimepoint.geolocation(location)
+    @lat = @latlng[0].round(3)
+    @lng = @latlng[1].round(3)
+
+    @queried_crimepoint = Crimepoint.find_by(latitude: @lat, longitude: @lng)
+
+		# @all_crimepoints = Crimepoint.return_crimepoints.to_json
 		# render json: @all_crimepoints
 	end
 
